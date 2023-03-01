4292f1d1dff4ebaa48fd93b6cae261f6cf0375b9
Krishi Wali <krishiwali@hotmail.co.uk>
2023-03-01 03:14:54 +0000

    Link home page buttons to corresponding pages

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,25 +1,33 @@
 import { Button } from '@mui/material'
+import { Box } from '@mui/system'
+import { useNavigate } from 'react-router'
 
 export default function Home() {
+  const navigate = useNavigate()
+
   return (
     <>
-    <div className="title">
+    <Box className="title">
       <h1>Turing Machine Simulator</h1>
-    </div>
-    <div className="buttons">
+    </Box>
+
+    <Box className="buttons">
       <Button variant='contained' sx={{
         aspectRatio: "1 / 1",
         width: "150px",
         borderRadius: "50%"
         }}
+        onClick={() => navigate("somewhere")}
       >Load a Turing Machine</Button>
+
       <Button variant='contained' sx={{
         aspectRatio: "1 / 1",
         width: "150px",
         borderRadius: "50%"
         }}
+        onClick={() => navigate("machines")}
       >Create a Turing Machine</Button>
-    </div>
+    </Box>
     </>
   )
 }
