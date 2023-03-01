ba4044bb2dfc047ef1d192400c9f1e259f16d4ad
Krishi Wali <krishiwali@hotmail.co.uk>
2023-03-01 03:18:21 +0000

    Add the MachineControls component to the Machines page

--- src/pages/Machines.jsx
+++ src/pages/Machines.jsx
@@ -1,3 +1,29 @@
+import { useState } from "react";
+import MachineControls from "../components/MachineControls";
+
 export default function Machines() {
-    return (<></>)
+  const [stopDisabled, setStopDisabled] = useState(true)
+  const [startDisabled, setStartDisabled] = useState(false)
+
+  const resetPressed = (initialValue) => {
+    console.log(initialValue)
+    setStartDisabled(false)
+    setStopDisabled(true)
+  }
+
+  const startPressed = () => {
+    setStartDisabled(true)
+    setStopDisabled(false)
+  }
+
+  const stopPressed = () => {
+    setStartDisabled(false)
+    setStopDisabled(true)
+  }
+
+  return (
+    <>
+    <MachineControls startDisabled={startDisabled} stopDisabled={stopDisabled} reset={resetPressed} start={startPressed} stop={stopPressed} />
+    </>
+  )
 }
