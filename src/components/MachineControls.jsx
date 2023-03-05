5e5047277f09dd83ea78aa8673dd966af4a2371d
Krishi Wali <krishiwali@hotmail.co.uk>
2023-03-05 02:22:27 +0000

    Modify grid breakpoints for MachineControls buttons

--- src/components/MachineControls.jsx
+++ src/components/MachineControls.jsx
@@ -5,13 +5,13 @@ export default function MachineControls({ startDisabled, stopDisabled, reset, st
     <Box>
       <TextField label="Initial Input" margin="normal" fullWidth onChange={(event) => setInitialValue(event.target.value)} />
       <Grid container className="machine-controls" spacing={1}>
-        <Grid item sm={4}>
+        <Grid item xs={4}>
           <Button variant="contained" color="info" fullWidth onClick={reset}>Reset</Button>
         </Grid>
-        <Grid item sm={4}>
+        <Grid item xs={4}>
           <Button variant="contained" color="success" fullWidth onClick={start} disabled={startDisabled}>Start</Button>
         </Grid>
-        <Grid item sm={4}>
+        <Grid item xs={4}>
           <Button variant="contained" color="error" fullWidth onClick={stop} disabled={stopDisabled}>Stop</Button>
         </Grid>
       </Grid>
