6d0340253678c33de8f24024e20189067e35019f
Krishi Wali <krishiwali@hotmail.co.uk>
2023-03-08 04:28:25 +0000

    Refactor Grid to a Stack in MachineControls

--- src/components/MachineControls.jsx
+++ src/components/MachineControls.jsx
@@ -1,5 +1,5 @@
 import { Lock, LockOpen } from "@mui/icons-material";
-import { Button, Grid, TextField, Box, IconButton } from "@mui/material";
+import { Button, TextField, Box, IconButton, Stack } from "@mui/material";
 
 export default function MachineControls({
   reset, start, stop, setInitialValue,
@@ -10,17 +10,11 @@ export default function MachineControls({
         {editorIsLocked ? <Lock sx={{ color: turingMachineIsRunning ? "" : "gold" }} /> : <LockOpen sx={{ color: "green" }} />}
       </IconButton>
       <TextField label="Initial Input" margin="normal" fullWidth onChange={(event) => setInitialValue(event.target.value)} disabled={!editorIsLocked} />
-      <Grid container className="machine-controls" spacing={1}>
-        <Grid item xs={4}>
-          <Button variant="contained" color="info" fullWidth onClick={reset} disabled={!editorIsLocked}>Reset</Button>
-        </Grid>
-        <Grid item xs={4}>
-          <Button variant="contained" color="success" fullWidth onClick={start} disabled={turingMachineIsRunning || !editorIsLocked}>Start</Button>
-        </Grid>
-        <Grid item xs={4}>
-          <Button variant="contained" color="error" fullWidth onClick={stop} disabled={!turingMachineIsRunning}>Stop</Button>
-        </Grid>
-      </Grid>
+      <Stack direction="row" spacing={1}>
+        <Button variant="contained" color="info" fullWidth onClick={reset} disabled={!editorIsLocked}>Reset</Button>
+        <Button variant="contained" color="success" fullWidth onClick={start} disabled={turingMachineIsRunning || !editorIsLocked}>Start</Button>
+        <Button variant="contained" color="error" fullWidth onClick={stop} disabled={!turingMachineIsRunning}>Stop</Button>
+      </Stack>
     </Box>
   )
 }
