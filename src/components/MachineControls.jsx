8fae42a48ce7d7e6b3916a015cc65244826fa198
Krishi Wali <krishiwali@hotmail.co.uk>
2023-03-12 22:31:02 +0000

    Add a slider to adjust turing machine computation speed to MachineControls

--- src/components/MachineControls.jsx
+++ src/components/MachineControls.jsx
@@ -1,17 +1,36 @@
 import { Lock, LockOpen } from "@mui/icons-material";
-import { Button, TextField, Box, IconButton, Stack } from "@mui/material";
+import { Button, TextField, Box, IconButton, Stack, Slider } from "@mui/material";
+
+const marks = [
+  {
+    value: -1,
+    label: "0.5x"
+  },
+  {
+    value: 0,
+    label: "1x",
+  },
+  {
+    value: 1,
+    label: "2x",
+  },
+  // {
+  //   value: 2,
+  //   label: "4x",
+  // },
+];
 
 export default function MachineControls({
-  reset, start, stop, lock, setInitialValue,
-  editorIsLocked, setEditorIsLocked, turingMachineIsRunning }) {
+  reset, start, stop, lock, setInitialValue, setSpeed,
+  editorIsLocked, turingMachineIsRunning }) {
   return (
     <Box>
-      <IconButton disabled={turingMachineIsRunning} onClick={() => {
-        if (!editorIsLocked) lock()
-        setEditorIsLocked(!editorIsLocked)
-      }}>
-        {editorIsLocked ? <Lock sx={{ color: turingMachineIsRunning ? "" : "gold" }} /> : <LockOpen sx={{ color: "green" }} />}
-      </IconButton>
+      <Stack direction="row">
+        <IconButton disabled={turingMachineIsRunning} onClick={lock}>
+          {editorIsLocked ? <Lock sx={{ color: turingMachineIsRunning ? "" : "gold" }} /> : <LockOpen sx={{ color: "green" }} />}
+        </IconButton>
+        <Slider defaultValue={0} sx={{ margin: "10px" }} min={-1} step={1} max={1} marks={marks} onChange={(event, newValue) => setSpeed(2 ** newValue)} />
+      </Stack>
       <TextField label="Initial Input" margin="normal" fullWidth onChange={(event) => setInitialValue(event.target.value)} disabled={!editorIsLocked} />
       <Stack direction="row" spacing={1}>
         <Button variant="contained" color="info" fullWidth onClick={reset} disabled={!editorIsLocked}>Reset</Button>
