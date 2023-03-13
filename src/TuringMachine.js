73923fa156359d9ea2594c3c499a1d603a0ce122
Krishi Wali <krishiwali@hotmail.co.uk>
2023-03-13 00:02:12 +0000

    Add a complete implementation for the internal Turing machine

--- src/TuringMachine.js
+++ src/TuringMachine.js
@@ -1,34 +1,42 @@
+import { OneWayInfiniteTape, TwoWayInfiniteTape } from "./Tape"
+import {immerable} from "immer"
+
 export default class TuringMachine {
-  constructor(selections, transitions, initialValue) {
-    this.initialValue = initialValue
+  [immerable] = true
+
+  constructor(selections, transitions, initialValue, oneWayInfiniteTape=true) {
     this.initial = selections["Initial State"]
     this.accept = selections["Accepting State"]
     this.reject = selections["Rejecting State"]
     this.transitions = transitions
+    this.oneWayInfiniteTape = oneWayInfiniteTape
 
     // Configuration (state) of the turing machine
-    this.currentState = this.initial
-    this.currentTapeHeadPosition = 0
-    this.tape = [...this.initialValue]
+    this.state = this.initial
+    this.headPosition = 0
+    this.tape = this.oneWayInfiniteTape ?
+      new OneWayInfiniteTape(initialValue) :
+      new TwoWayInfiniteTape(initialValue)
   }
 
   getConfiguration() {
     return {
-      state: this.currentState,
-      headPosition: this.currentTapeHeadPosition,
+      state: this.state,
+      headPosition: this.headPosition,
       tape: this.tape
     }
   }
 
   setConfiguration(configuration) {
-    this.currentState = configuration.state
-    this.currentTapeHeadPosition = configuration.headPosition
+    this.state = configuration.state
+    this.headPosition = configuration.headPosition
     this.tape = configuration.tape
   }
 
-  getTransitions(readValue) {
-    return this.transitions.filter(
-      transition => transition.state === this.currentState && transition.read === readValue
+  getTransitions() {
+    return this.transitions.filter(transition =>
+      transition.state === this.state &&
+      transition.read === this.tape.readCell(this.headPosition)
     )
   }
 
@@ -37,49 +45,30 @@ export default class TuringMachine {
       transition => transition.id === transitionId
     )
 
-    this.tape[this.currentTapeHeadPosition] = transition.write
-    this.currentState = transition.nextState
+    this.tape.writeCell(this.headPosition, transition.write)
+    this.state = transition.nextState
 
     switch (transition.move) {
       case 0:
-        if (this.currentTapeHeadPosition !== 0) {
-          this.currentTapeHeadPosition -= 1
+        if (!this.oneWayInfiniteTape || this.headPosition !== 0) {
+          this.headPosition -= 1
         }
         break
       case 1:
-        if (this.currentTapeHeadPosition === this.tape.length - 1) {
-          this.tape.push("")
-        }
-        this.currentTapeHeadPosition += 1
+        this.headPosition += 1
         break
       default:
         break;
     }
 
-    return this.getConfiguration()
+    // return this.getConfiguration()
   }
 
-  // resetTape(initialValue) {}
+  reset(initialValue) {
+    this.state = this.initial
+    this.headPosition = 0
+    this.tape = this.oneWayInfiniteTape ?
+      new OneWayInfiniteTape(initialValue) :
+      new TwoWayInfiniteTape(initialValue)
+  }
 }
-
-var tm = new TuringMachine({
-  "Initial State": "s1",
-  "Accepting State": "",
-  "Rejecting State": "",
-}, [
-  {
-    id: 0, move: 1, nextState: "s2", read: "", state: "s1", write: "0"
-  },
-  {
-    id: 1, move: 1, nextState: "s3", read: "", state: "s2", write: ""
-  },
-  {
-    id: 2, move: 1, nextState: "s4", read: "", state: "s3", write: "1"
-  },
-  {
-    id: 3, move: 1, nextState: "s1", read: "", state: "s4", write: ""
-  },
-], "")
-
-// Need to get the next transitions given an input
-// Need to choose the next transition
