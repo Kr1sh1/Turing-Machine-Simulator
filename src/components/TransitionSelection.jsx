81239820727ff4724de63c4d9e78e4ee4bf45c21
Krishi Wali <krishiwali@hotmail.co.uk>
2023-03-17 02:14:09 +0000

    Add TransitionSelection component for non-determinism

---
+++ src/components/TransitionSelection.jsx
@@ -0,0 +1,12 @@
+import { Button, Stack } from "@mui/material";
+
+export default function TransitionSelection({ availableTransitions, transitionSelected }) {
+  return (
+    <Stack spacing={1}>
+      Available Transitions
+      {availableTransitions.map((transition) => (
+        <Button variant="contained" onClick={() => transitionSelected(transition.id)}>{transition.write} {transition.move} {transition.nextState}</Button>
+      ))}
+    </Stack>
+  )
+}
