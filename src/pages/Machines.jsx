9e13f410a11505105e6ed4fdd58d994a850173b1
Krishi Wali <krishiwali@hotmail.co.uk>
2023-03-17 02:26:19 +0000

    Refactor Turing machine logic out of Machines page

--- src/pages/Machines.jsx
+++ src/pages/Machines.jsx
@@ -1,38 +1,12 @@
-import { Box, Snackbar } from "@mui/material";
-import { useCallback, useEffect, useRef, useState } from "react";
-import MachineControls from "../components/MachineControls";
-import TuringMachine from "../TuringMachine"
+import { Box, FormControl, FormControlLabel, FormLabel, IconButton, Radio, RadioGroup, Snackbar } from "@mui/material";
+import { useState } from "react";
 import StateSelection from "../components/StateSelection";
 import TransitionTable from "../components/TransitionTable";
-import Tape from "../components/Tape";
-import { useImmer } from "use-immer";
-import { SimulatorState, StateType } from "../Enums";
-import Status from "../components/Status";
-import ComputationTree from "../components/ComputationTree";
-import { ReactFlowProvider } from "reactflow";
-
-const makeNode = (id, label) => {
-  return {
-    id: `${id}`,
-    position: { x: 0, y: 0 },
-    data: { label: label },
-    deletable: false,
-    sourcePosition: "right",
-    targetPosition: "left",
-  }
-}
-
-const makeEdge = (id, source, target) => {
-  return {
-    id: `${id}`,
-    source: `${source}`,
-    target: `${target}`,
-    animated: true,
-  }
-}
+import { StateType } from "../Enums";
+import Simulator from "../components/Simulator";
+import { Lock, LockOpen } from "@mui/icons-material";
 
 export default function Machines() {
-  const [initialValue, setInitialValue] = useState("");
   const [transitions, setTransitions] = useState([
     {
       id: 0,
@@ -58,16 +32,7 @@ export default function Machines() {
   })
   const [editorIsLocked, setEditorIsLocked] = useState(false)
   const [activeTransitionID, setActiveTransitionID] = useState(-1)
-  const [simulatorStatus, setSimulatorStatus] = useState(SimulatorState.PAUSED)
-  const [turingMachine, updateTuringMachine] = useImmer(new TuringMachine(selections, transitions, initialValue, true))
-  const tickerID = useRef()
-  const [ticks, setTicks] = useState(0)
-  const [speed, setSpeed] = useState(1)
   const [oneWayInfiniteTape, setOneWayInfiniteTape] = useState(true)
-  const [nodes, setNodes] = useState([])
-  const [edges, setEdges] = useState([])
-  const [counter, setCounter] = useState(0)
-  const [activeNodeId, setActiveNodeId] = useState({})
 
   const uniqueStates = new Set(
     transitions.reduce(
@@ -90,56 +55,6 @@ export default function Machines() {
   })
   if (selectionChanged) setSelections(selectionsCopy)
 
-  useEffect(() => {
-    if (simulatorStatus !== SimulatorState.RUNNING) return
-
-    const changeActiveNodeClass = (className) => {
-      setNodes(nodes =>
-        nodes.map(node => {
-          if (node.id === activeNodeId) {
-            node.className = className
-          }
-          return node
-        })
-      )
-    }
-
-    const availableTransitions = turingMachine.getTransitions()
-    switch (availableTransitions.length) {
-      case 0:
-        setSimulatorStatus(SimulatorState.TERMINATED)
-        let className = ""
-        if (turingMachine.state === selections[StateType.ACCEPT]) className = "acceptNode"
-        else if (turingMachine.state === selections[StateType.HALT]) className = "haltNode"
-        else className = "rejectNode"
-
-        changeActiveNodeClass(className)
-
-        setActiveTransitionID(-1)
-        return
-      case 1:
-        setActiveTransitionID(availableTransitions[0].id)
-        updateTuringMachine(draft => {
-          draft.performTransition(availableTransitions[0].id)
-        })
-        const newNode = makeNode(counter, "Not First Node")
-        setNodes([...nodes, newNode])
-        setCounter(count => count + 1)
-        setEdges([...edges, makeEdge(counter, activeNodeId, newNode.id)])
-        setActiveNodeId(newNode.id)
-        break
-      default:
-        changeActiveNodeClass("nondetNode")
-        break
-    }
-
-    const id = setTimeout(() => setTicks(ticks + 1) , 1000 / speed)
-    tickerID.current = id
-    return () => clearTimeout(id)
-
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [ticks])
-
   const lockPressed = () => {
     if (editorIsLocked) {
       setEditorIsLocked(!editorIsLocked)
@@ -151,13 +66,6 @@ export default function Machines() {
       const haltHasExit = transitions.find(transition => halting.includes(transition.state))
       if (initSet && invalids.length === 0 && !haltHasExit) {
         setEditorIsLocked(!editorIsLocked)
-        updateTuringMachine(new TuringMachine(selections, transitions, initialValue, oneWayInfiniteTape))
-        setSimulatorStatus(SimulatorState.PAUSED)
-        const newNode = {...makeNode(counter, "First Node"), type: "input"}
-        setNodes([newNode])
-        setActiveNodeId(newNode.id)
-        setCounter(count => count + 1)
-        setEdges([])
       }
     }
   }
@@ -167,32 +75,6 @@ export default function Machines() {
       transition.state === "" ||
       transition.nextState === ""
     )
-  }
-
-  // TODO: Reset Turing machine state to the one at this node
-  const nodeClicked = useCallback((event, node) => {}, [])
-
-  const resetPressed = () => {
-    stopPressed()
-    setActiveTransitionID(-1)
-    updateTuringMachine(draft => {
-      draft.reset(initialValue, oneWayInfiniteTape)
-    })
-    const newNode = {...makeNode(counter, "First Node"), type: "input"}
-    setNodes([newNode])
-    setActiveNodeId(newNode.id)
-    setCounter(count => count + 1)
-    setEdges([])
-  }
-
-  const startPressed = () => {
-    setSimulatorStatus(SimulatorState.RUNNING)
-    setTicks(ticks + 1)
-  }
-
-  const stopPressed = () => {
-    setSimulatorStatus(SimulatorState.PAUSED)
-    clearTimeout(tickerID)
   }
 
   return (
@@ -212,25 +94,24 @@ export default function Machines() {
         activeTransitionID={activeTransitionID} />
     </Box>
 
-    <Box sx={{ display: "flex" }}>
-      <MachineControls
-        reset={resetPressed}
-        start={startPressed}
-        stop={stopPressed}
-        lock={lockPressed}
-        setInitialValue={setInitialValue}
-        setSpeed={setSpeed}
-        editorIsLocked={editorIsLocked}
-        setOneWayInfiniteTape={setOneWayInfiniteTape}
-        simulatorStatus={simulatorStatus} />
-      <Tape
-        configuration={turingMachine.getConfiguration()} />
-    </Box>
-    <Status simulatorStatus={simulatorStatus} currentState={turingMachine.state} selections={selections} />
+    <FormControl disabled={editorIsLocked}>
+      <FormLabel>Type of infinite tape</FormLabel>
+      <RadioGroup defaultValue="1" onChange={event => setOneWayInfiniteTape(event.target.value === "1")}>
+        <FormControlLabel value="1" control={<Radio />} label="One-way" />
+        <FormControlLabel value="2" control={<Radio />} label="Two-way" />
+      </RadioGroup>
+    </FormControl>
+
+    <IconButton onClick={lockPressed}>
+      {editorIsLocked ? <Lock sx={{ color: "gold" }} /> : <LockOpen sx={{ color: "green" }} />}
+    </IconButton>
+
     {editorIsLocked &&
-      <ReactFlowProvider>
-        <ComputationTree rawNodes={nodes} rawEdges={edges} activeNodeId={activeNodeId} simulatorStatus={simulatorStatus} nodeClicked={nodeClicked} />
-      </ReactFlowProvider>
+    <Simulator
+      selections={selections}
+      transitions={transitions}
+      oneWayInfiniteTape={oneWayInfiniteTape}
+      setActiveTransitionID={setActiveTransitionID} />
     }
     </>
   )
