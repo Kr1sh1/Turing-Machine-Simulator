2521767ded6618ad7f1cfc61f8c42aea9b00f1c9
Krishi Wali <krishiwali@hotmail.co.uk>
2023-03-19 00:46:38 +0000

    Keep case for transition buttons

--- src/components/TransitionSelection.jsx
+++ src/components/TransitionSelection.jsx
@@ -1,16 +1,24 @@
 import { Casino } from "@mui/icons-material";
-import { Button,  FormControlLabel, IconButton, Stack, Switch, Typography } from "@mui/material";
+import { Button,  FormControlLabel, IconButton, Stack, styled, Switch, Typography } from "@mui/material";
 import { Box } from "@mui/system";
+
+const CaseSensitiveButton = styled(Button)(`
+  text-transform: none;
+`);
 
 export default function TransitionSelection({ availableTransitions, transitionSelected, alwaysPickRandomly, pickRandom, changeAlwaysPick }) {
   return (
-    <Box className="component" sx={{ display: "flex", flexDirection: "column", backgroundColor: "teal", color: "white", padding: "10px", justifyContent: "space-between" }}>
+    <Box className="component" sx={{ display: "flex", flexDirection: "column", flexBasis: "325px", alignItems: "center", backgroundColor: "teal", color: "white", padding: "10px", justifyContent: "space-between" }}>
       <Stack spacing={1}>
         <Typography variant="body1" textAlign={"center"} noWrap>
           Available Transitions
         </Typography>
         {availableTransitions.map((transition) => (
-          <Button key={transition.id} variant="contained" color="warning" onClick={() => transitionSelected(transition.id)}>{transition.write} {transition.move} {transition.nextState}</Button>
+          <CaseSensitiveButton fullWidth key={transition.id} variant="contained" color="warning" onClick={() => transitionSelected(transition.id)}>
+            <Typography>
+              Write: {transition.write} | Move: {transition.move} | Next State: {transition.nextState}
+            </Typography>
+          </CaseSensitiveButton>
         ))}
       </Stack>
       <Box sx={{ display: "flex" }}>
