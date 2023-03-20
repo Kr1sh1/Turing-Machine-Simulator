82264546d6de572ea463016b49f653d665be19f9
Krishi Wali <krishiwali@hotmail.co.uk>
2023-03-20 04:49:12 +0000

    Pretty up the home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,28 +5,39 @@ export default function Home() {
   const navigate = useNavigate()
 
   return (
-    <>
-    <Box className="title">
-      <h1>Turing Machine Simulator</h1>
-    </Box>
+    <Box sx={{ alignSelf: "center", display: "flex",
+               flexDirection: "column", width: "100%",
+               height: "100%", textAlign: "center",
+               justifyContent: "center", borderRadius: "10px",
+               backgroundColor: "brown" }}>
 
-    <Box className="buttons">
-      <Button variant='contained' sx={{
-        aspectRatio: "1 / 1",
-        width: "150px",
-        borderRadius: "50%"
-        }}
-        onClick={() => navigate("somewhere")}
-      >Load a Turing Machine</Button>
 
-      <Button variant='contained' sx={{
-        aspectRatio: "1 / 1",
-        width: "150px",
-        borderRadius: "50%"
-        }}
-        onClick={() => navigate("machines")}
-      >Create a Turing Machine</Button>
+      <Box className="title" sx={{ color: "white" }}>
+        <h1>Turing Machine Simulator</h1>
+      </Box>
+
+      <Box className="homeButtons" sx={{ position: "relative", width: "325px", height: "300px", margin: "0 auto" }}>
+        <Button variant='contained' sx={{
+          left: "0",
+          top: "0",
+          }}
+          onClick={() => navigate("somewhere")}
+        >Load an example</Button>
+
+        <Button variant='contained' sx={{
+          right: "0",
+          top: "0",
+          }}
+          onClick={() => navigate("somewhere")}
+        >Load a file</Button>
+
+        <Button variant='contained' sx={{
+          left: "calc(50% - 75px)",
+          bottom: "0"
+          }}
+          onClick={() => navigate("machines")}
+        >Create a Turing Machine</Button>
+      </Box>
     </Box>
-    </>
   )
 }
